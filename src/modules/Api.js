8bfa13931effea9d90496d4ea5bc0a6260e3c061
Abdo9826 <102296263+Abdo9826@users.noreply.github.com>
2022-06-20 19:35:24 +0300

    add moduls files

---
+++ src/modules/Api.js
@@ -0,0 +1,170 @@
+import {
+  DisplayPopup,
+  DisplayCards,
+  Counter,
+  CountComment,
+  DisplayComments,
+  CounterLikes,
+} from './Functions.js';
+import { modal } from './Dom.js';
+
+export default class Api {
+  constructor() {
+    this.InvolvementApiEP = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+    this.InvolvementAppID = '2WkPU2aU9V462U8puyKk';
+    this.FreeMealEP = 'https://www.themealdb.com/api/json/v1/';
+  }
+
+  GetMealInfos = async (id) => {
+    await fetch(`${this.FreeMealEP}/1/categories.php`)
+      .then((response) => response.json())
+      .then((json) => {
+        json.categories.forEach((item) => {
+          if (item.idCategory === id) {
+            this.GetExamples(item, id);
+          }
+        });
+      });
+  };
+
+  GetExamples = async (item) => {
+    const modal = document.querySelector('.popup-container');
+    modal.innerHTML = ' <div class="spinner" ><i class="fas fa-spinner fa-spin fa-5x"></i></div>';
+    await fetch(`${this.FreeMealEP}/1/filter.php?c=${item.strCategory}`)
+      .then((response) => response.json())
+      .then((json) => {
+        DisplayPopup(item, json);
+        this.CountComments(item.idCategory);
+        this.DisplayComm(item.idCategory);
+
+        const form = document.querySelector('.form');
+        form.addEventListener('submit', (e) => {
+          e.preventDefault();
+          const username = document.getElementById('name').value;
+
+          const comment = document.querySelector('#comment').value;
+          const button = document.querySelector('.submit').id;
+          if (username && comment) {
+            const newComment = {
+              username,
+              comment,
+              item_id: button,
+            };
+            this.AddComment(newComment);
+          }
+        });
+      });
+  };
+
+  GetStats = async (products) => {
+    Counter(products);
+    this.CountLikes();
+  };
+
+  GetMeals = async () => {
+    document.querySelector('.cards').innerHTML = ' <div class="spinner" ><i class="fas fa-spinner fa-spin fa-5x"></i></div>';
+    await fetch(`${this.FreeMealEP}/1/categories.php`)
+      .then((response) => response.json())
+      .then((json) => {
+        DisplayCards(json.categories);
+        this.GetStats(json.categories);
+        const likeBtn = document.querySelectorAll('.fa-heart');
+        const likeCount = document.querySelectorAll('.likes-counter');
+        likeBtn.forEach((btn) => {
+          btn.addEventListener('click', () => {
+            if (!btn.classList.contains('liked')) {
+              btn.classList.add('liked');
+              likeCount.forEach((count) => {
+                if (count.id === btn.id) {
+                  this.AddLike(count.id);
+                  count.innerHTML = Number(count.innerText) + 1;
+                }
+              });
+            }
+          });
+        });
+        const comment = document.querySelectorAll('.comment');
+        comment.forEach((item) => {
+          item.addEventListener('click', () => {
+            modal.classList.toggle('hide');
+            this.GetMealInfos(item.id);
+          });
+        });
+      });
+  };
+
+  AddComment = async (data) => {
+    document.getElementById('name').setAttribute('disabled', '');
+    document.querySelector('#comment').setAttribute('disabled', '');
+    document.querySelector('.submit').innerHTML = ' <i class="fas fa-spinner fa-spin fa-1x"></i>';
+    await fetch(
+      `${this.InvolvementApiEP}apps/${this.InvolvementAppID}/comments`,
+      {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      },
+    ).then(() => {
+      document.getElementById('name').removeAttribute('disabled');
+      document.querySelector('#comment').removeAttribute('disabled');
+      document.querySelector('.submit').innerHTML = 'submit';
+    });
+    this.DisplayComm(data.item_id);
+  };
+
+  DisplayComm = async (data) => {
+    const CommentList = document.querySelector('.comments');
+    CommentList.innerHTML = ' <i class="fas fa-spinner fa-spin fa-2x"></i>';
+    await fetch(
+      `${this.InvolvementApiEP}apps/${this.InvolvementAppID}/comments?item_id=${data}`,
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.constructor === Array) {
+          DisplayComments(json);
+        } else {
+          CommentList.innerHTML = '';
+        }
+      });
+  };
+
+  AddLike = async (id) => {
+    await fetch(
+      `${this.InvolvementApiEP}apps/${this.InvolvementAppID}/likes/`,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          item_id: id,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      },
+    );
+  };
+
+   CountLikes = async () => {
+     await fetch(
+       `${this.InvolvementApiEP}apps/${this.InvolvementAppID}/likes/`,
+     ).then((response) => response.json())
+       .then((json) => {
+         if (json.constructor === Array) {
+           CounterLikes(json);
+         }
+       });
+   };
+
+  CountComments = async (data) => {
+    await fetch(
+      `${this.InvolvementApiEP}apps/${this.InvolvementAppID}/comments?item_id=${data}`,
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.constructor === Array) {
+          CountComment(json);
+        }
+      });
+  };
+}
---
+++ src/modules/Functions.js
@@ -0,0 +1,159 @@
+import { modal, Cards, ProductsCounter } from './Dom.js';
+
+function DisplayPopup(data, examples) {
+  modal.innerHTML = ` <div class="closeBtn">X</div>
+  <div class="popup">
+            <div class="popup-header" >
+                
+                    <div class="img-popcontainer">
+                        <div class="img-pophold">
+                            <div class="img">
+                                <img src="${data.strCategoryThumb}">
+                            </div>
+                        </div>
+                    </div>
+                <div class="popup-title">${data.strCategory}</div>
+            </div>
+
+            <div class="popup-details">
+              
+                <div class="details">
+                    <div class="images">
+                    </div>
+                    <hr />
+                   <div class="details-text">
+                   ${data.strCategoryDescription}
+                    </div>
+                </div>
+                <div class="popup-comments">
+                    <div class="container-comments">
+                        <form class="form">
+                    
+                    <textarea id="comment" type="text" placeholder="Comment" required/></textarea>
+                    <div class="form-header"><input required id="name" type="text" placeholder="Name" /> <button class="submit" id="${data.idCategory}">submit</button></div>
+                        </form>
+                        <div class="Comment-counter">
+                        Total Comments(0)
+                        </div>
+                     <hr/>
+                     
+                    <div class="comment-list">
+                        
+                        <ul class="comments">
+                                        
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+   </div>`;
+  const images = document.querySelector('.images');
+  examples.meals.slice(0, 3).forEach((element) => {
+    const div = document.createElement('div');
+    div.classList.add('single-product-example');
+    div.innerHTML = ` 
+    <div class="img-popholdpop">
+        <div class="imgex">
+            <img src=${element.strMealThumb}>
+
+        </div>
+    </div>
+    <p class="title-example">${element.strMeal.substr(0, 15)}</p>
+`;
+    images.appendChild(div);
+  });
+
+  const closeBtn = document.querySelector('.closeBtn');
+
+  closeBtn.addEventListener('click', () => {
+    modal.classList.toggle('hide');
+  });
+}
+
+function DisplayCards(data) {
+  document.querySelector('.cards').innerHTML = '';
+  data.forEach((element) => {
+    Cards.innerHTML += `
+          <div class="card">
+          <div class="img-container">
+              <div class="img-hold">
+                  <div class="img">
+                      <img src="${element.strCategoryThumb}">
+                  </div>
+              </div>//   const LikesCounter = document.querySelectorAll('.likes-counter');
+          </div>
+          <div class="title-container">
+              <h3>${element.strCategory}</h3>
+              <div class="interactions">
+              <div></div>       <div><i id="${
+  element.idCategory
+}" class="fa-solid fa-heart fa-lg"></i> <b id="${
+  element.idCategory
+}" class="likes-counter"></b> </div>
+              </div>
+              <div>
+              ${element.strCategoryDescription.substr(0, 50)}...
+              </div>
+              
+          <button id="${
+  element.idCategory
+}" class="button comment"><i class="fa-solid fa-comments"></i> Comments</button>
+          </div>
+  
+      </div>
+          `;
+  });
+}
+
+function calculateComments(data) {
+  const msg = `Total Comments(${data.length})`;
+  return msg;
+}
+
+function CountComment(data) {
+  const CommentCounter = document.querySelector('.Comment-counter');
+  CommentCounter.innerHTML = calculateComments(data);
+}
+
+function DisplayComments(data) {
+  const commentSection = document.querySelector('.comments');
+  let comment = '';
+  data.forEach((item) => {
+    comment += `<li class="single-comment">
+      <img class="user" src="https://www.w3schools.com/howto/img_avatar.png" alt="user" >
+      <b class="user-comment">${item.username}: ${item.comment}</b>
+      </li>`;
+  });
+  commentSection.innerHTML = comment;
+  CountComment(data);
+}
+
+function countProducts(data) {
+  return data.length;
+}
+
+function Counter(data) {
+  ProductsCounter.innerHTML = countProducts(data);
+}
+
+function CounterLikes(data) {
+  const likeCount = document.querySelectorAll('.likes-counter');
+  likeCount.forEach((item) => {
+    data.forEach((likes) => {
+      if (item.id === likes.item_id) {
+        item.innerHTML = likes.likes;
+      }
+    });
+  });
+}
+
+export {
+  DisplayCards,
+  DisplayPopup,
+  Counter,
+  DisplayComments,
+  CountComment,
+  CounterLikes,
+  calculateComments,
+  countProducts,
+};
