574d0d4ecb765404f78cf6c60af34a60712833a1
Abdo9826 <102296263+Abdo9826@users.noreply.github.com>
2022-06-20 17:32:40 +0300

    add modules Js

---
+++ src/modules/Dom.js
@@ -0,0 +1,7 @@
+const modal = document.querySelector('.popup-container');
+const comment = document.querySelectorAll('.comment');
+const Cards = document.querySelector('.cards');
+const ProductsCounter = document.querySelector('.product-counter');
+export {
+  modal, ProductsCounter, comment, Cards,
+};
